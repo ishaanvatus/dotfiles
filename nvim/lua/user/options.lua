d3cf2c04a017bb2bcc971143b4da66c729d45e0a
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2023-08-30 17:47:15 +0000

    Add files via upload

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,9 @@
+--theme
+vim.opt.termguicolors=true
+vim.cmd [[colorscheme kanagawa-dragon]]
+--options 
+vim.opt.clipboard=unnamedplus 
+vim.opt.relativenumber=true
+vim.opt.tabstop=4
+vim.opt.expandtab=true
+vim.opt.shiftwidth=4
