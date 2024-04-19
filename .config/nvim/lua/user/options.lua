4af0de2c6f805fa08681c20a12cf0d0f650a460c
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2024-04-19 23:57:48 +0530

    change: "set number" in nvim

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -2,9 +2,11 @@
 vim.opt.termguicolors=true
 vim.cmd [[colorscheme gruvbox]]
 vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
+vim.cmd [[set autoindent]]
 --options 
 vim.opt.clipboard=unnamedplus 
 vim.opt.relativenumber=true
+vim.opt.number=true
 vim.opt.tabstop=4
 vim.opt.expandtab=true
 vim.opt.shiftwidth=4
