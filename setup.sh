e90274e56980edea7616b0cb608ca41a44af4c64
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2024-12-27 17:43:41 -0500

    clean: remove crud

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo dnf install iwlwifi-mvm-firmware sway fuzzel waybar sddm stow git ranger neovim mpv neofetch foot keepassxc qbittorrent
-rm .bashrc
-rm .gitconfig
-sudo systemctl enable sddm
-sudo systemctl set-default graphical.target
-mkdir desktop documents downloads music pictures videos
-stow .
