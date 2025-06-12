1a20099e80ac2b8ca116ed17166cf31a15b999e1
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2025-06-12 15:20:07 +0530

    add: "latest texlive path vars"

--- .bashrc.d/exports.sh
+++ .bashrc.d/exports.sh
@@ -4,6 +4,7 @@ export EDITOR=nvim
 export GIT_EDITOR=nvim
 export HISTSIZE= 
 export HISTFILESIZE=
-#export PICO_SDK_PATH=/opt/pico-sdk
-
+export MANPATH=/usr/local/texlive/2025/texmf-dist/doc/man:$MANPATH 
+export INFOPATH=/usr/local/texlive/2025/texmf-dist/doc/info:$INFOPATH
+export PATH=/usr/local/texlive/2025/bin/x86_64-linux:$PATH
 
