77ea361a36ca5bea501b3a6173c24768a401ccf0
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2025-11-07 00:08:15 +0530

    change:jxl maxxing

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
- 
+DEVOUR_CMD=swallow
 alias vi='nvim'
 alias vim='nvim'
-alias sxiv='nsxiv'
-#alias sxiv='gobble nsxiv'
-#alias mpv='gobble mpv'
-#alias vlc='gobble vlc'
-#alias zathura='gobble zathura'
+alias sxiv='$DEVOUR_CMD nsxiv'
+alias mpv='$DEVOUR_CMD mpv'
+alias vlc='$DEVOUR_CMD vlc'
+alias zathura='$DEVOUR_CMD zathura'
