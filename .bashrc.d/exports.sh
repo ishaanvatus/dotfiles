94979334ebcdd5cd369f49c42665996572887084
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2025-11-08 09:17:18 +0530

    fix:sleep

--- .bashrc.d/exports.sh
+++ .bashrc.d/exports.sh
@@ -7,4 +7,6 @@ export HISTFILESIZE=
 export MANPATH=/usr/local/texlive/2025/texmf-dist/doc/man:$MANPATH 
 export INFOPATH=/usr/local/texlive/2025/texmf-dist/doc/info:$INFOPATH
 export PATH=/usr/local/texlive/2025/bin/x86_64-linux:$PATH
-
+export GOPATH=$HOME/.go
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin
