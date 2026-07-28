e9ee46370989fb31905b180dda6d97175f3eeab4
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2026-07-29 03:36:36 +0530

    regression: トラッシュ, Na2Cu(SO4)2•2H2O
    Keyboard too good I'm committed to sway, ik Kröhnkite exists (SHA: 373e08f6ba891444d46556b810148b4867ba8601)
    
    "If you can’t do something, then don’t. Focus on what you can do" - ゼロから, a3 ch5
    
    only gripe with sway is the picom focus thing I had in i3/dwm (SHA: b353b95c0a49d09ae5bdcdbfd4df17b19f0c1dc5)

--- toolchains.sh
+++ toolchains.sh
@@ -2,10 +2,10 @@
 set -eo pipefail
 
 echo "don't add anything to path for rustup"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo "don't add anything to path or enable completions for deno"
-curl -fsSL https://deno.land/install.sh | sh -s -- --no-modify-path
+curl -fsSL https://deno.land/install.sh | sh
 source $HOME/.bashrc || true
 deno completions bash > $HOME/.bashrc.d/user/deno.bash
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | PROFILE=/dev/null bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 echo "remove lines added to .bashrc by nvm"
