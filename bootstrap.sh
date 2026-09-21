bd70a881155b509862afa4fb2f585c322f4be43c
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2026-09-22 03:24:26 +0530

    fix: rpmfusion config and multimedia
    https://rpmfusion.org/Configuration
    https://rpmfusion.org/Howto/Multimedia

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,7 +37,7 @@ source $HOME/.bashrc || true
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y config-manager setopt fedora-cisco-openh264.enabled=1
 sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing
-sudo dnf -y install @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin --allowerasing
+sudo dnf -y install @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf -y install intel-media-driver 
 
 # packages
