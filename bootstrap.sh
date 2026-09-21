dc8ff3a78672d6bf4fb04454656e1e68bdc97603
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2026-09-22 03:46:22 +0530

    fix: libheif-freeworld and libheif-ffmpeg
    https://github.com/rpmfusion/libheif-freeworld/commit/3379ac8e553bafad73d86551035bcd300ab5f878#diff-99c1edbe2ce2724ce6d10f1f05c7edbe091ea104999f29b2f008ea329377ab14
    
    Problem: problem with installed package
      - package libheif-ffmpeg-1.23.4-6.fc44.x86_64 from updates requires libheif(x86-64) = 1.23.4-6.fc44, but none of the providers can be installed
      - installed package libheif-ffmpeg-1.23.4-6.fc44.x86_64 requires libheif(x86-64) = 1.23.4-6.fc44, but none of the providers can be installed
      - package libheif-freeworld-1.21.2-3.fc44.x86_64 from rpmfusion-free requires libheif(x86-64) = 1.21.2, but none of the providers can be installed
      - conflicting requests
      - cannot install both libheif-1.23.4-6.fc44.x86_64 from updates and libheif-1.21.2-1.fc44.x86_64 from fedora
      - cannot install both libheif-1.21.2-1.fc44.x86_64 from fedora and libheif-1.23.4-6.fc44.x86_64 from @System
    You can try to add to command line:
      --skip-broken to skip uninstallable packages
    
    bug reports (and a reddit post)
    https://bugzilla.rpmfusion.org/show_bug.cgi?id=7540
    https://www.reddit.com/r/Fedora/comments/139vcid/problem_with_libheif/
    
    similar one from 2023:
    https://bugzilla.rpmfusion.org/show_bug.cgi?id=6670

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,8 +36,10 @@ source $HOME/.bashrc || true
 ## rpm fusion
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y config-manager setopt fedora-cisco-openh264.enabled=1
+sudo dnf -y update @core
+sudo dnf -y install rpmfusion-\*-appstream-data
 sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing
-sudo dnf -y install @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf -y install @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin,libheif-freeworld,libheif-ffmpeg
 sudo dnf -y install intel-media-driver 
 
 # packages
